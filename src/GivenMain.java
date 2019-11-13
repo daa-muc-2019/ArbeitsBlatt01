e8d23a3334d16ee5a2b0f8001ec69f4d1c1dba85
daa-muc-2019 <daa.muc.2019@gmail.com>
2019-11-14 00:06:02 +0100

    add GivenMain

---
+++ src/GivenMain.java
@@ -0,0 +1,49 @@
+
+public class GivenMain{
+
+
+ public static void main(String[] args) {
+
+  Sheep sheep = new Sheep(170, 100, Colour.RED);
+  	
+
+  sheep.draw();
+  
+  //schnupfen
+  sheep.sniff();
+  
+  //mit den Ohren wackeln
+  sheep.wagEar();
+  
+  //essen
+  sheep.eat();
+  
+  //schlafen
+  sheep.sleep();
+  
+  //aufwachen
+  sheep.wakeUp();
+  
+  //verdaut
+  sheep.digest();
+  
+  //wackelt mit dem Schwanz
+  sheep.wagTail();
+  
+  //zwingert
+  sheep.wink();
+ 
+
+ }
+}
+
+
+
+
+
+
+
+
+
+
+
